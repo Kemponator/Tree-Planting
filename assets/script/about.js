c55f95bd6c7d2500bc91adbd5a20da90a3a3cf4b
Sam Kemp <samrowankemp@gmail.com>
2023-07-04 16:30:05 +0100

    updates to about.html and about.js

--- assets/script/about.js
+++ assets/script/about.js
@@ -9,7 +9,7 @@ bioButtonFrances.addEventListener("click", handleFrancesBioClick);
 function handleFrancesBioClick() {
   if (francesBioVisible) {
     bioButtonFrances.textContent = "read bio";
-    bioVisible = false;
+    francesBioVisible = false;
   } else {
     bioButtonFrances.textContent = "hide bio";
     francesBioVisible = true;
@@ -18,4 +18,24 @@ function handleFrancesBioClick() {
 
   francesTopImage.classList.toggle("visible");
   francesTopImage.classList.toggle("hidden");
+}
+
+let samBioVisible = false;
+
+const bioButtonSam = document.getElementById("bio-button-sam");
+
+bioButtonSam.addEventListener("click", handleSamBioClick);
+
+function handleSamBioClick() {
+  if (samBioVisible) {
+    bioButtonSam.textContent = "read bio";
+    samBioVisible = false;
+  } else {
+    bioButtonSam.textContent = "hide bio";
+    samBioVisible = true;
+  }
+  const samTopImage = document.getElementById("sam-top-image");
+
+  samTopImage.classList.toggle("visible");
+  samTopImage.classList.toggle("hidden");
 }
